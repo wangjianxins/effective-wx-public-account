1fa465d2d5608fe66f4d1f90cb64e450e10b7f94
731461008@qq.com <WJX18042690933>
2018-04-11 00:20:25 +0800

    Initial commit

---
+++ src/main/java/com/effective/wechat/public_account/PublicAccountApplication.java
@@ -0,0 +1,12 @@
+package com.effective.wechat.public_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicAccountApplication.class, args);
+    }
+}
